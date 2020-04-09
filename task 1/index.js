29fe810864cb8f8d0b27e44ab5d45788c4af5042
Юзаев Ильфат <wartvader@gmail.com>
2020-04-09 18:34:18 +0400

    test

--- task 1/index.js
+++ task 1/index.js
@@ -93,11 +93,9 @@ function HTMLGenerator(id, modificate)
     let data = data1[0].question;
     let title = "<div>" + data1[0].question + "</div>";
     if(id != json.length)
-    {
-        data = '';
         data = DataGenerator(id, data1);
-        
-    }
+    else 
+        data = '';
     console.log(id);
     let fp = fullParamPath + "\\quest" + data1[0].id + ".html"; //file path
     let file = htmlwb4_start + title + data + htmlwb4_end;
